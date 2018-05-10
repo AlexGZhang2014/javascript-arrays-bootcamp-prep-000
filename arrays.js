16ba5d145d5bb23d3fc479572707d1e03e12e5cf
Alex Zhang <alexgzhang92@gmail.com>
2018-05-10 23:20:52 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -6,10 +6,12 @@ var chocolateBars = [
   ]
 
 function addElementToBeginningOfArray(array, element) {
-  array = [element, ...array];
+  var newArray = [element, ...array];
+  return newArray;
 }
 
 function destructivelyAddElementToBeginningOfArray(array, element) {
-  
+  array.unshift(element);
+  return array;
 }
 
