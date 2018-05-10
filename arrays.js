75986cb83d442564c48b009bfad140e7c6845dc0
Alex Zhang <alexgzhang92@gmail.com>
2018-05-10 23:25:23 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -15,3 +15,13 @@ function destructivelyAddElementToBeginningOfArray(array, element) {
   return array;
 }
 
+function addElementToEndOfArray (array, element) {
+  var newArray = [...array, element];
+  return newArray;
+}
+
+function destructivelyAddElementToEndOfArray(array, element) {
+  array.push(element);
+  return array;
+}
+
